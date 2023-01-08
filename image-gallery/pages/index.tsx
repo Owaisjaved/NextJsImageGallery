2dc0383056a1b2c09b5264759ffd6bdf71c68377
OwaisJaved <owaisjaved954@gmail.com>
2023-01-08 14:03:30 +0100

    first commit

---
+++ image-gallery/pages/index.tsx
@@ -0,0 +1,30 @@
+export default function Gallery (){
+ return ( <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        "Image"
+      </div>
+    </div>
+    )
+}
+
+function Image() {
+  return(
+    <a href='#' className="group">
+      <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+        <Image
+          // alt=""
+          // src={image.imageSrc}
+          // className={cn(
+          //   'duration-700 ease-in-out group-hover:opacity-75',
+          //   isLoading
+          //     ? 'scale-110 blur-2xl grayscale'
+          //     : 'scale-100 blur-0 grayscale-0'
+          // )}
+          // onLoadingComplete={() => setLoading(false)}
+        />
+      </div>
+      <h3 className="mt-4 text-sm text-gray-700">{'image name'}</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">{'image username'}</p>
+    </a>
+  )
+}
